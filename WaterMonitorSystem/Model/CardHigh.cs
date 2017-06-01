aca9c121d3b8fd0347b08e8270084c667008f3af
kqz <374565452@qq.com>
2017-06-01 21:26:32 +0800

    @2017-6-1 21：25 解决客户那里，不能够开高级用户卡的问题

---
+++ WaterMonitorSystem/Model/CardHigh.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maticsoft.Model
+{
+    /// <summary>
+    /// 高级用户卡
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class CardHigh
+    {
+        /// <summary>
+        /// Id
+        /// </summary>	
+        [DataMember]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// IC卡序列号
+        /// </summary>	
+        [DataMember]
+        public string SerialNumber { get; set; }
+
+        /// <summary>
+        /// 开卡操作人id
+        /// </summary>	
+        [DataMember]
+        public long OpenUserId { get; set; }
+
+        /// <summary>
+        /// 开卡操作人名字
+        /// </summary>	
+        [DataMember]
+        public string OpenUserName { get; set; }
+
+        /// <summary>
+        /// 开卡地址
+        /// </summary>	
+        [DataMember]
+        public string OpenAddress { get; set; }
+
+        /// <summary>
+        /// 开卡时间
+        /// </summary>	
+        [DataMember]
+        public DateTime OpenTime { get; set; }
+    }
+}
---
+++ WaterMonitorSystem/Module/CardHighLogModule.cs
@@ -0,0 +1,52 @@
+﻿using DBUtility;
+using Maticsoft.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    //add by kqz 2017-5-9 17:14
+    public class CardHighLogModule
+    {
+        public static long AddCardClearLog(CardHighLog cardClearLog)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("insert into CardHighLog(");
+            strSql.Append("SerialNumber,LogUserId,LogUserName,LogAddress,LogTime,LogType,LogContent)");
+            strSql.Append(" values (");
+            strSql.Append("@SerialNumber,@LogUserId,@LogUserName,@LogAddress,@LogTime,@LogType,@LogContent)");
+            strSql.Append(";select @@IDENTITY");
+            SqlParameter[] parameters = {
+					new SqlParameter("@SerialNumber", SqlDbType.NVarChar,50),
+					new SqlParameter("@LogUserId", SqlDbType.BigInt,8),
+					new SqlParameter("@LogUserName", SqlDbType.NVarChar,50),
+					new SqlParameter("@LogAddress", SqlDbType.NVarChar,200),
+					new SqlParameter("@LogTime", SqlDbType.DateTime),
+					new SqlParameter("@LogType", SqlDbType.NVarChar,50),
+					new SqlParameter("@LogContent", SqlDbType.NVarChar,-1)};
+            parameters[0].Value = cardClearLog.SerialNumber;
+            parameters[1].Value = cardClearLog.LogUserId;
+            parameters[2].Value = cardClearLog.LogUserName;
+            parameters[3].Value = cardClearLog.LogAddress;
+            parameters[4].Value = cardClearLog.LogTime;
+            parameters[5].Value = cardClearLog.LogType;
+            parameters[6].Value = cardClearLog.LogContent;
+
+            object obj = DbHelperSQL.GetSingle(strSql.ToString(), parameters);
+            if (obj == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Convert.ToInt64(obj);
+            }
+        }
+
+    }
+}
